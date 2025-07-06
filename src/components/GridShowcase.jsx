f258f149263478b740e8d0b831d7e981e345c8ee
lucas <lucas@Lucas-MacBook-Air-2.local>
2025-07-06 12:35:19 +0800

    Added Olist Section

--- src/components/GridShowcase.jsx
+++ src/components/GridShowcase.jsx
@@ -11,11 +11,17 @@ import spl5 from '../assets/spl5.png';
 import spl6 from '../assets/spl6.png';
 import lstm from '../assets/LSTM.png';
 import xgboost from '../assets/XGBoost.png';
+import o1 from '../assets/o1.png';
+import o2 from '../assets/o2.png';
+import o3 from '../assets/o3.png';
+import o4 from '../assets/o4.png';
+import o5 from '../assets/o5.png';
 
 const projects = [
     {name: 'Credit Card Fraud Detection Model', href: "https://fraud-detection-model-lucasang.streamlit.app/", badge: 'ML', details: 'PostgreSQL • DBeaver • Scikit-Learn • Seaborn • Pandas • NumPy • Streamlit • Python', images: [ml1,ml2,ml3,ml4,ml5], alt: "Project Picture"},
     {name: 'Statistical Analysis and Performance Visualisation', href: "https://lucasang.notion.site/Finding-the-Best-Forward-in-the-SPL-2024-25-7a8b0315b45d413e8e760d18cb12bf22", badge: 'Data Viz', details: 'Plotly • Matplotlib • Pandas • NumPy • Python', images: [spl1, spl2, spl3, spl4, spl5, spl6], alt: "Project Picture"},
     {name: 'AMZN Stock Price Forecasting', badge: 'ML', details: 'XGBoost • LSTM • Scikit-Learn • TensorFlow • Keras • Statsmodels • Scikit-Learn • Seaborn • Matplotlib • Pandas • NumPy • Jupyter', images: [xgboost, lstm], alt: "Project Picture"},
+    {name: 'Olist Business Optimisation', badge: 'Data Viz', details: 'MySQL • Tableau', images: [o1,o2,o3,o4,o5], alt: "Presentation"},
 ]
 
 export default function GridShowcase() {
